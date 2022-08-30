292ece548d6d7e6c249ab7d4bb185c0dbb1c6518
shinyewon <won980@naver.com>
2022-08-30 22:49:24 +0900

    NDA css 수정

--- portfolio/project/nda/css/script.js
+++ portfolio/project/nda/css/script.js
@@ -1,3 +1,10 @@
+// 모달창 닫기
+$(document).mouseup(function (e) {
+  if ($("button").has(e.target).length === 0) {
+    $("#modal").hide();
+  }
+});
+
 // 서브메뉴
 $(document).ready(function () {
   $("#side_menu > ul > .sub_menu").hide();
@@ -7,7 +14,7 @@ $(document).ready(function () {
 });
 
 //  NDA history 슬라이드 배너
-$(document).ready(function () {
+/* $(document).ready(function () {
   var $banner = $(".banner").find("ul");
 
   var $bannerWidth = $banner.children().outerWidth();
@@ -40,4 +47,37 @@ $(document).ready(function () {
       $(this).css("left", 0);
     });
   }
+}); */
+
+/* $(".slide-next").click(function () {
+  if (photo < 3) {
+    $("#dance_slide").css({ transform: `translateX(-${photo}00vw)` });
+    photo += 1;
+  } else {
+    $("#dance_slide").css({ transform: `transelateX(0)` });
+    photo = 1;
+  }
 });
+$(".slide-prev").click(function () {
+  if (photo > 1) {
+    $("#dance_slide").css({ transform: `translateX(-${photo - 2}00vw)` });
+    photo -= 1;
+  } else {
+    $("#dance_slide").css({ transform: `translateX(-200vw)` });
+    photo = 3;
+  }
+}); */
+/* var currentIndex = 0;
+setInterval(function () {
+  if (currentIndex < 5) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+  var slidePosition = currentIndex * -100 + "%";
+  $("#dance_slide").animate({ left: slidePosition }, 1000);
+}, 3000);
+ */
+// $("#dance_slide li").slick();
+
+//current position
