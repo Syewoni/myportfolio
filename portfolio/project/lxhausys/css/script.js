80c157d82b476e7ba3d3242da39d1b6428708ed3
shinyewon <won980@naver.com>
2023-03-21 16:54:51 +0900

    lx 모바일 수정

--- portfolio/project/lxhausys/css/script.js
+++ portfolio/project/lxhausys/css/script.js
@@ -1,45 +1,72 @@
 // 헤더 이미지 슬라이드
-const slideItem = document.querySelectorAll('#title_img .slide_item')
+const slideItem = document.querySelectorAll("#title_img .slide_item");
 const prevBtn = document.querySelector("#btns .prev");
 const nextBtn = document.querySelector("#btns .next");
 
 function autoSlide() {
   let i = 0;
   prevBtn.addEventListener("click", () => {
-    slideItem[i].classList.remove('active');
+    slideItem[i].classList.remove("active");
     i--;
 
     if (i < 0) {
       i = slideItem.length - 1;
     }
-    slideItem[i].classList.add("active")
+    slideItem[i].classList.add("active");
   });
 
-  nextBtn.addEventListener('click', () => {
-    slideItem[i].classList.remove('active');
+  nextBtn.addEventListener("click", () => {
+    slideItem[i].classList.remove("active");
     i++;
     if (i >= slideItem.length) {
       i = 0;
     }
-    slideItem[i].classList.add('active');
+    slideItem[i].classList.add("active");
   });
 }
 
 setInterval(function () {
-  nextBtn.click()
+  nextBtn.click();
 }, 5000);
 
 autoSlide();
 
+// 메뉴 슬라이드
+ $(document).ready(function(){
+		$("nav ul.sub_menu").hide();
+		$("nav ul#menu li").click(function(){
+			$("ul",this).slideToggle("fast");
+		});
+	});
+ 
+// 버튼클릭
+ $(function(){
+ 
+ $("#slide-open").click(function()    {
+   if($("#burgur").hasClass('on')){
+     $("#burgur").removeClass('on');
+     $("#slide").removeClass('on');
+    
+   } else{
+     $("#burgur").addClass('on');
+    $("#slide").addClass('on'); 
+   }
+ });
+ 
+});
+   
+
+
+
 // 제품소개 swiper
-/* const swiper = new Swiper(".icon_swipper", {
-  slidesPerView: 1, // 화면에 보여지는 이미지 갯수
-  // spaceBetween: 30, 이미지 사이 간격
+/* new Swiper(".swiper-container", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loopFillGroupWithBlank: true,
+
   loop: true,
-  autoplay: {
-    // 자동실행
-    delay: 2500, // 2.5초 대기 후 이동
-  },
+
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -48,42 +75,94 @@ autoSlide();
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  breakpoints: {
-    // 스크린 사이즈가 415보다 크거나 같으면 실행
-    415: {
-      slidesPerView: 2,
-    },
-  },
-}); */
-new Swiper('.swiper-container', {
 
-  slidesPerView: 3, // 동시에 보여줄 슬라이드 갯수
-  spaceBetween: 30, // 슬라이드간 간격
-  slidesPerGroup: 3, // 그룹으로 묶을 수, slidesPerView 와 같은 값을 지정하는게 좋음
-
-  // 그룹수가 맞지 않을 경우 빈칸으로 메우기
-  // 3개가 나와야 되는데 1개만 있다면 2개는 빈칸으로 채워서 3개를 만듬
-  loopFillGroupWithBlank: true,
-
-  loop: true, // 무한 반복
-
-  pagination: { // 페이징
-    el: '.swiper-pagination',
-    clickable: true, // 페이징을 클릭하면 해당 영역으로 이동, 필요시 지정해 줘야 기능 작동
-  },
-  navigation: { // 네비게이션
-    nextEl: '.swiper-button-next', // 다음 버튼 클래스명
-    prevEl: '.swiper-button-prev', // 이번 버튼 클래스명
-  },
-
-  breakpoints: {
+   breakpoints: {
     415: {
       slidesPerView: 3,
       spaceBetween: 20,
     },
-    1024: {
+    1920: {
       slidesPerView: 5,
-      spaceBetween: 30,
+      spaceBetween: 20,
     },
   },
-});
+  
+}); */
+$(document).ready(function () {
+  slideAct();
+})
+
+function slideAct(){
+  var view = 0; //보이는 슬라이드 개수
+  var realInx = 0; //현재 페이지
+  var swiper = undefined;
+
+  //디바이스 체크
+  var winWChk = '';
+  $(window).on('load resize', function (){
+    var winW = window.innerWidth;
+    if(winWChk != 'mo' && winW <= 768){ //모바일 버전으로 전환할 때
+      slideList()
+      winWChk = 'mo';
+    }
+    if(winWChk != 'pc' && winW >= 769){ //PC 버전으로 전환할 때
+      slideList()
+      winWChk = 'pc';
+    }
+  })  
+
+  function slideList(){
+    //리스트 초기화
+    if ($('.slider .list').parent().hasClass('swiper-slide')){
+      $('.slider .swiper-slide-duplicate').remove();
+      $('.slider .list').unwrap('swiper-slide');
+    }
+
+    //보이는 슬라이드 개수 설정
+    if (window.innerWidth > 768){ //PC 버전
+      view = 6;
+    }else{ //mobile 버전
+      view = 6;
+    } 
+
+    //리스트 그룹 생성 (swiper-slide element 추가)
+    var num = 0;
+    $('.slider').find('.list').each(function(i) {
+      $(this).addClass('list'+(Math.floor((i+view)/view)));
+      num = Math.floor((i+view)/view)
+    }).promise().done(function(){
+      for (var i = 1; i < num+1; i++) {
+        $('.slider').find('.list'+i+'').wrapAll('<div class="swiper-slide"></div>');
+        $('.slider').find('.list'+i+'').removeClass('list'+i+'');
+      }
+    });
+
+    sliderStart()
+  }
+
+  //슬라이드 시작
+  function sliderStart(){
+    //슬라이드 초기화
+    if(swiper != undefined) {
+      swiper.destroy();
+      swiper == undefined;
+    }
+
+    //슬라이드 실행
+    swiper = new Swiper('.slider .inner', {
+      slidesPerView: 1,
+      initialSlide :Math.floor(realInx/view),
+      resistanceRatio : 0,
+      loop:true,
+      navigation: {
+        nextEl: $('.slider').find('.swiper-next'),
+        prevEl: $('.slider').find('.swiper-prev'),
+      },
+      on: {
+        slideChange: function () {
+          realInx = this.realIndex*view
+        }
+      },
+    });
+  }
+} 
--- portfolio/project/nda/css/script.js
+++ portfolio/project/nda/css/script.js
@@ -9,6 +9,65 @@ $(document).mouseup(function (e) {
 $(document).ready(function () {
   $("#side_menu > ul > .sub_menu").hide();
   $("#side_menu > ul > li").click(function () {
-    $("ul", this).slideToggle("fast");
+    $("ul", this).slideToggle();
   });
+});
+
+// 가로로 움직이는 텍스트
+$(function () {
+  $(".marquee").marquee({
+    duplicated: true,
+    delayBeforeStart: 0,
+    pauseOnHover: true,
+  });
+});
+
+// ytPlayer
+
+$(function () {
+  // play list 선언
+  var videos = [
+    { videoURL: "X9HNUQWfzvg", containment: 'self', autoPlay: true, mute: true, startAt: 0, opacity: 1, loop: false, showControls: false, useOnMobile: true, stopMovieOnBlur: false, abundance: 0 },
+    { videoURL: "9703gEdtZm4", containment: 'self', autoPlay: true, mute: true, startAt: 0, opacity: 1, loop: false, showControls: false, useOnMobile: true, stopMovieOnBlur: false, abundance: 0 },
+    { videoURL: "V4OurL6FiQQ", containment: 'self', autoPlay: true, mute: true, startAt: 0, opacity: 1, loop: false, showControls: false, useOnMobile: true, stopMovieOnBlur: false, abundance: 0 }
+  ];
+
+  // play list 불러오기
+  $("#myPlayer1").YTPlaylist(videos, false);
+});
+
+$(function () {
+  // play list 선언
+  var videos = [
+    { videoURL: "DorzZhmlLN0", containment: 'self', autoPlay: true, mute: true, startAt: 0, opacity: 1, loop: false, showControls: false, useOnMobile: true, stopMovieOnBlur: false, abundance: 0 },
+    { videoURL: "Dt7YPeilct8", containment: 'self', autoPlay: true, mute: true, startAt: 0, opacity: 1, loop: false, showControls: false, useOnMobile: true, stopMovieOnBlur: false, abundance: 0 },
+    { videoURL: "hXlFbhIIewg", containment: 'self', autoPlay: true, mute: true, startAt: 0, opacity: 1, loop: false, showControls: false, useOnMobile: true, stopMovieOnBlur: false, abundance: 0 }
+  ];
+
+  // play list 불러오기
+  $("#myPlayer2").YTPlaylist(videos, false);
+});
+
+$(function () {
+  // play list 선언
+  var videos = [
+    { videoURL: "Doz9s4S42KQ", containment: 'self', autoPlay: true, mute: true, startAt: 0, opacity: 1, loop: false, showControls: false, useOnMobile: true, stopMovieOnBlur: false, abundance: 0 },
+    { videoURL: "krD441cvA-4", containment: 'self', autoPlay: true, mute: true, startAt: 0, opacity: 1, loop: false, showControls: false, useOnMobile: true, stopMovieOnBlur: false, abundance: 0 },
+    { videoURL: "7GotVgNcrFQ", containment: 'self', autoPlay: true, mute: true, startAt: 0, opacity: 1, loop: false, showControls: false, useOnMobile: true, stopMovieOnBlur: false, abundance: 0 }
+  ];
+
+  // play list 불러오기
+  $("#myPlayer3").YTPlaylist(videos, false);
+});
+
+$(function () {
+  // play list 선언
+  var videos = [
+    { videoURL: "RqUUZckJVv4", containment: 'self', autoPlay: true, mute: true, startAt: 0, opacity: 1, loop: false, showControls: false, useOnMobile: true, stopMovieOnBlur: false, abundance: 0 },
+    { videoURL: "TkX8toqt02I", containment: 'self', autoPlay: true, mute: true, startAt: 0, opacity: 1, loop: false, showControls: false, useOnMobile: true, stopMovieOnBlur: false, abundance: 0 },
+    { videoURL: "Amso2aDY0tU", containment: 'self', autoPlay: true, mute: true, startAt: 0, opacity: 1, loop: false, showControls: false, useOnMobile: true, stopMovieOnBlur: false, abundance: 0 }
+  ];
+
+  // play list 불러오기
+  $("#myPlayer4").YTPlaylist(videos, false);
 });
